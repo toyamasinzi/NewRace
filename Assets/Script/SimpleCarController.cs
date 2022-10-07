5a6608f3e39c76747713ad79f36c6d69cc0b244c
sinzitoyama <yamat422@gmail.com>
2022-10-07 22:19:37 +0900

    車作成進歩

--- Assets/Script/SimpleCarController.cs
+++ Assets/Script/SimpleCarController.cs
@@ -5,23 +5,29 @@ using System.Collections.Generic;
 [System.Serializable]
 public class AxleInfo
 {
-    public WheelCollider _leftWheel;
-    public WheelCollider _rightWheel;
-    public bool _motor;
-    public bool _steering;
+    /// <summary>  プロパティ </summary>
+    [SerializeField] WheelCollider _leftWheel;
+    [HideInInspector] public WheelCollider LeftWheel { get => _leftWheel; }
+    [SerializeField] WheelCollider _rightWheel;
+    [HideInInspector]public WheelCollider RightWheel { get => _rightWheel;}
+    [SerializeField] bool _motor;
+    [HideInInspector] public bool Motor { get => _motor; }
+    [SerializeField] bool _steering;
+    [HideInInspector] public bool Steering { get => _steering; }
 }
 
 public class SimpleCarController : MonoBehaviour
 {
-    public List<AxleInfo> axleInfos;
-    [SerializeField] float maxMotorTorque;//アクセルに力を加えるためのトルク数
-    [SerializeField] float maxSteeringAngle;//ステアリングホイールの回転角度
+    [SerializeField] List<AxleInfo> _axleInfos;
+    [SerializeField] float _maxMotorTorque;//アクセルに力を加えるためのトルク数
+    [SerializeField] float _maxSteeringAngle;//ステアリングホイールの回転角度
 
 
-    //タイヤの回転を表現する
+    //タイヤの回転を表現する。目に見えるタイヤの動き
     // 対応する視覚的なホイールを見つけます
     // Transform を正しく適用します
-    /*public void ApplyLocalPositionToVisuals(WheelCollider collider)
+    public void ApplyLocalPositionToVisuals(WheelCollider collider)
+
     {
         if (collider.transform.childCount == 0)//子オブジェクトが0だったら戻す　エラーを出さないための処理
         {
@@ -36,27 +42,27 @@ public class SimpleCarController : MonoBehaviour
 
         visualWheel.transform.position = position;//子のオブジェクトの位置をワールド座標に変換
         visualWheel.transform.rotation = rotation;//子のオブジェクトの角度をワールド座標に変換
-    }*/
+    }
 
     public void FixedUpdate()
     {
-        float motor = maxMotorTorque * Input.GetAxis("Vertical");
-        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+        float motor = _maxMotorTorque * Input.GetAxis("Vertical");
+        float steering = _maxSteeringAngle * Input.GetAxis("Horizontal");
 
-        foreach (AxleInfo axleInfo in axleInfos)
+        foreach (AxleInfo axleInfo in _axleInfos)
         {
-            if (axleInfo._steering)//steeringのbool型を判定して左右のホイールのsteerAngleに反映
+            if (axleInfo.Steering)//steeringのbool型を判定して左右のホイールのsteerAngleに反映　タイヤの曲がるときの角度
             {
-                axleInfo._leftWheel.steerAngle = steering;
-                axleInfo._rightWheel.steerAngle = steering;
+                axleInfo.LeftWheel.steerAngle = steering;
+                axleInfo.RightWheel.steerAngle = steering;
             }
-            if (axleInfo._motor)//motorのbool型を判定して左右のホイールのmotorTorqueに反映
+            if (axleInfo.Motor)//motorのbool型を判定して左右のホイールのmotorTorqueに反映　タイヤの前進
             {
-                axleInfo._leftWheel.motorTorque = motor;
-                axleInfo._rightWheel.motorTorque = motor;
+                axleInfo.LeftWheel.motorTorque = motor;
+                axleInfo.RightWheel.motorTorque = motor;
             }
-            /*ApplyLocalPositionToVisuals(axleInfo._leftWheel);
-            ApplyLocalPositionToVisuals(axleInfo._rightWheel);*/
+            ApplyLocalPositionToVisuals(axleInfo.LeftWheel);
+            ApplyLocalPositionToVisuals(axleInfo.RightWheel);
         }
     }
 }
