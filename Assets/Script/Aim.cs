6806d322ecf12f207eba1ed31905053ec3356fde
sinzitoyama <yamat422@gmail.com>
2022-11-14 17:07:19 +0900

    使わない部分削除

--- Assets/Script/Aim.cs
+++ Assets/Script/Aim.cs
@@ -1,22 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Aim : MonoBehaviour
 {
     public Mouse _mousePos;
     private Vector3 _targetPos;
-
     [SerializeField] float _speed;
-    private float _step;
 
     void Update()
     {
         _mousePos._targetPos.y = transform.position.y;
         _targetPos = _mousePos._targetPos;
-        _step = _speed * Time.deltaTime;
         Vector3 targetDir = _targetPos - transform.position;
-        Vector3 moveDir = Vector3.RotateTowards(transform.forward, targetDir, _step, 0f);
+        Vector3 moveDir = Vector3.RotateTowards(transform.forward, targetDir, _speed * Time.deltaTime, 0f);
         transform.rotation = Quaternion.LookRotation(moveDir);
     }
 }
--- Assets/Script/Mouse.cs
+++ Assets/Script/Mouse.cs
@@ -1,45 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Mouse : MonoBehaviour
 {
     public Vector3 _targetPos;
+    //public Vector3 TargetPos { get => _targetPos; set => _targetPos = value; }
     [SerializeField] Image _aimImage;
 
     void Update()
     {
-        // 「マウスの位置」と「照準器の位置」を同期させる。
         transform.position = Input.mousePosition;
-
         RaycastHit hit;
-
-        // MainCameraからマウスの位置にRayを飛ばす
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
 
         if (Physics.Raycast(ray, out hit))
         {
-            // RayがColliderと衝突した地点の座標を取得
             _targetPos = hit.point;
-            print(_targetPos);
-
-            if (hit.transform.CompareTag("Enemy"))
-            {
-                // 照準器を赤色に変化させる。
-                _aimImage.color = new Color(1.0f, 0.0f, 0.0f, 1.0f);
-            }
-            else
-            {
-                // 照準器の色は白
-                _aimImage.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
-            }
-        }
-        else
-        {
-            // 照準器の色は白
-            _aimImage.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
         }
     }
 }
